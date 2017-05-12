8fd38eb279ac93061336ccf50a28342df4feaac5
Jake F - @samouri <samouri@users.noreply.github.com>
2017-05-12 15:04:00 -0400

    Copy fork located at samouri/prettier

---
+++ README.md
@@ -0,0 +1,9 @@
+# WP-Prettier
+
+This is a fork of prettier that add in all of the extra spaces we use in wp-calypso.
+It is forked off of prettier v1.1.
+
+In order to install run 
+```
+npm i --save-optional "git+https://github.com/samouri/prettier.git"
+```
---
+++ tests/switch/empty_lines.js
@@ -0,0 +1,53 @@
+switch (foo) {
+  case "bar":
+    doSomething();
+
+  case "baz":
+    doOtherThing();
+}
+
+switch (foo) {
+
+  case "bar":
+    doSomething();
+
+  case "baz":
+    doOtherThing();
+}
+
+switch (foo) {
+  case "bar":
+    doSomething();
+
+  case "baz":
+    doOtherThing();
+
+}
+
+switch (foo) {
+  case "bar":
+    doSomething();
+
+
+
+  case "baz":
+    doOtherThing();
+}
+
+switch (x) {
+  case y:
+    call();
+
+    break;
+
+  case z:
+    call();
+
+    break;
+}
+
+switch (a) {
+  case b:
+    if (1) {};
+    c;
+}
